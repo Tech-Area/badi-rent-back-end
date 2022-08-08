aa09ce4fa272047b4ad3dc9bfb73a2ce86ef1137
Ciprian <steffanciprian@gmail.com>
2022-08-07 18:36:44 -0700

    First commit

---
+++ src/main/java/com/techarea/badirent/BadiRentBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.techarea.badirent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadiRentBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BadiRentBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techarea/badirent/BadiRentBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techarea.badirent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadiRentBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
