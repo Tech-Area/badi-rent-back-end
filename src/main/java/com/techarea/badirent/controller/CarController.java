d2adf38e576ed083a1837e08f1293247f08bef39
Ciprian <steffanciprian@gmail.com>
2022-08-16 11:49:41 -0700

    added a property in application.properties

--- src/main/java/com/techarea/badirent/controller/CarController.java
+++ src/main/java/com/techarea/badirent/controller/CarController.java
@@ -8,13 +8,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
 @ControllerAdvice
+@CrossOrigin(origins = "*")
 public class CarController {
     private final CarService carService;
 
@@ -28,12 +28,10 @@ public class CarController {
         return new ResponseEntity<>(carService.getAll(), HttpStatus.OK);
     }
 
-    @PostMapping( produces = { "application/json" })
-
+    @PostMapping(produces = {"application/json"})
 //    @PreAuthorize("hasRole('ADMIN')")
 //    @PreAuthorize("permitAll()")
     public ResponseEntity<CarDto> save(@RequestPart("carDto") String carDto, @RequestPart("file") MultipartFile imageFile) throws IOException {
-        return new ResponseEntity<>(carService.save(carDto,imageFile), HttpStatus.CREATED);
-//        return new ResponseEntity<>(carService.save(carDto,imageFile),HttpStatus.CREATED);
+        return new ResponseEntity<>(carService.save(carDto, imageFile), HttpStatus.CREATED);
     }
 }
